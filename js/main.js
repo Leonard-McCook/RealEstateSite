1b81835383aae1294ff12e4704d88f20d4173035
Leonard-McCook <leonard.mccookcarr@outlook.com>
2022-05-28 18:32:28 -0400

    Build JavaScript header scroll function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+    const header = document.getElementById('header')
+    if(this.scrollY >= 50) header.classList.add('scroll-header');
+}
 
+window.addEventListener('scroll', scrollHeader)
 
 /*=============== SWIPER POPULAR ===============*/
 
